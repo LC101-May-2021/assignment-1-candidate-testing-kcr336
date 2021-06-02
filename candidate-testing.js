b4e14ec53c551bf2f3c10c2c041874f4d96f6bea
Karen Beisel <>
2021-06-02 18:45:49 +0000

    I added the two arrays for questions and correct answers

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,25 +3,47 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let askForName = "What is your name?";
-let candidateName= input.question(askForName);
-
-console.log("Hello "+candidateName+", welcome to our test. Let's get started.");
-
+let candidateName="";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 const question = "Who was the first American woman in space?";
 const correctAnswer = "Sally Ride";
-let candidateAnswer = input.question(question);
+let candidateAnswer="";
+const questions = ["Who was the first American woman in space?", "True or false: 5 kilometer == 5000 meters?", "(5 + 3)/2 * 10 = ?", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = ["Sally Ride", "true", 	"40", "Trajectory", "3"];
+let candidateAnswers = [];
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+
+function askForName() {
+  // TODO 1.1b: Ask for candidate's name //
+let askForName = "What is your name?";
+let candidateName= input.question(askForName);
+
+return candidateName;
+}
+
+
+function askQuestion(candidateName) {
+  // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+console.log("Hello "+candidateName+", welcome to our test. Let's get started.");
+let candidateAnswer = input.question(question);
 if (candidateAnswer === correctAnswer) {
   console.log("Question one is correct.")
 }else {
   console.log("Question one is incorrect.")
 }
+}
+
+function gradeQuiz(candidateAnswers) {
+
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 
-/*
+  let grade;
+  
+
+  return grade;
+}
+
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
@@ -29,7 +51,7 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
-*/
+
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
 module.exports = {
@@ -37,9 +59,9 @@ module.exports = {
   question: question,
   correctAnswer: correctAnswer,
   candidateAnswer: candidateAnswer,
-  //questions: questions,
-  //correctAnswers: correctAnswers,
-  //candidateAnswers: candidateAnswers,
-  //gradeQuiz: gradeQuiz,
-  //runProgram: runProgram
+  questions: questions,
+  correctAnswers: correctAnswers,
+  candidateAnswers: candidateAnswers,
+  gradeQuiz: gradeQuiz,
+  runProgram: runProgram
 };
